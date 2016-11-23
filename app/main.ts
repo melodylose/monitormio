e4c7b759b25806e0b41ec394ce5e7f59eb11ae05
melodylose <jsdklose@hotmail.com>
2016-11-23 14:20:32 +0800

    加入typescript

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,19 @@
-var electron = require('electron');
-var app = electron.app;
+import * as electron from 'electron';
+import {app, BrowserWindow} from 'electron';
 
-var BrowserWindow = electron.BrowserWindow;
-
-var mainWindow = null;
+var mainWindow: Electron.BrowserWindow = null;
 
 app.on('window-all-closed', function () {
     if (process.platform != 'darwin') {
         app.quit();        
-    }
+    } 
 });
 
 app.on('ready', function () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
+
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
